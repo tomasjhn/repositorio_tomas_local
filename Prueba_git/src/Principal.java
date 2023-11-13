b099717dbdeac90c72316a1259add3810bf26105
tomas <tomasjhn@educastur.org>
2023-11-13 12:17:41 +0100

    segunda actualización

--- Prueba_git/src/Principal.java
+++ Prueba_git/src/Principal.java
@@ -4,6 +4,7 @@ public class Principal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola Mundo");
+		System.out.println("esta es una prueba de git");
 	}
 
 }
